f0aa79cd1fae9a1272691a4c156764bba76db4b4
SchneeHertz <snowhzhang@gmail.com>
2022-06-07 11:34:20 +0800

    create folder loader

---
+++ fileLoader/folder.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const {promisify} = require('util')
+const { nanoid } = require('nanoid')
+
+let getFolderlist = async (libraryPath)=>{
+
+}
+
+let solveBookTypeFolder = async (folderpath, TEMP_PATH, COVER_PATH)=>{
+
+}
+
+let getImageListFromFolder = async (folderpath, VIEWER_PATH)=>{
+
+}
+
+module.exports = {
+  getFolderlist,
+  solveBookTypeFolder,
+  getImageListFromFolder
+}

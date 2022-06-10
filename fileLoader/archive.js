b7a5c794f1b57af60a9d08a9cac8b890e082ca9c
SchneeHertz <snowhzhang@gmail.com>
2022-06-10 16:57:58 +0800

    partial update archive

--- fileLoader/archive.js
+++ fileLoader/archive.js
@@ -4,6 +4,7 @@ const glob = require('glob')
 const { promisify } = require('util')
 const { nanoid } = require('nanoid')
 const { spawn } = require('child_process')
+const _ = require('lodash')
 
 const _7z = path.join(process.cwd(), 'resources/extraResources/7z.exe')
 
@@ -18,11 +19,16 @@ let getArchivelist = async (libraryPath)=>{
 
 let solveBookTypeArchive = async (filepath, TEMP_PATH, COVER_PATH)=>{
   let tempFolder = path.join(TEMP_PATH, nanoid())
-  await spawnPromise(_7z, ['x', filepath, '-o' + tempFolder])
-  let imageList = await promisify(glob)('**/*.@(jpg|jpeg|png|gif|webp|avif)', {
-    cwd: tempFolder,
-    nocase: true
-  })
+  let output = await spawnPromise(_7z, ['l', filepath, '-slt'])
+  let pathlist = _.filter(output.join(',').split(/\r\n/), s=>_.startsWith(s, 'Path'))
+  pathlist = pathlist.map(p=>/(?<== ).*$/.exec(p)[0])
+  let imageList = _.filter(pathlist, p=>['.jpg','.jpeg','.png','.gif','.webp','.avif'].includes(path.extname(p).toLowerCase()))
+  console.log(imageList)
+  // await spawnPromise(_7z, ['x', filepath, '-o' + tempFolder])
+  // let imageList = await promisify(glob)('**/*.@(jpg|jpeg|png|gif|webp|avif)', {
+  //   cwd: tempFolder,
+  //   nocase: true
+  // })
   imageList = imageList.sort((a,b)=>a.localeCompare(b, undefined, {numeric: true, sensitivity: 'base'}))
   let targetFile
   let targetFilePath
@@ -30,8 +36,11 @@ let solveBookTypeArchive = async (filepath, TEMP_PATH, COVER_PATH)=>{
   let coverPath
   if (imageList.length > 8) {
     targetFile = imageList[7]
+    console.log(await spawnPromise(_7z, ['x', filepath, '-o'+tempFolder, targetFile]))
+    await spawnPromise(_7z, ['x', filepath, '-o'+tempFolder, imageList[0]])
   } else {
     targetFile = imageList[0]
+    await spawnPromise(_7z, ['x', filepath, '-o'+tempFolder, '-r', imageList[0]])
   }
   targetFilePath = path.join(TEMP_PATH, nanoid() + path.extname(targetFile))
   await fs.promises.copyFile(path.join(tempFolder, targetFile), targetFilePath)
@@ -57,14 +66,18 @@ let getImageListFromArchive = async (filepath, VIEWER_PATH)=>{
 let spawnPromise = (commmand, argument)=>{
   return new Promise((resolve, reject)=>{
     const spawned = spawn(commmand, argument)
+    let output = []
     spawned.on('error', data=>{
       reject(data)
     })
     spawned.on('exit', code=>{
       if (code === 0) {
-        return resolve()
+        return resolve(output)
       }
       return reject('close code is ' + code)
+    })
+    spawned.stdout.on('data', data=>{
+      output.push(data.toString())
     })
   })
 }

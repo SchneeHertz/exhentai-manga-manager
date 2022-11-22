968f1882aa1223aa71a050cab754e0113791dee5
SchneeHertz <snowhzhang@gmail.com>
2022-11-22 09:27:12 +0800

    update readme

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import App from './App.vue'

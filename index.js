d99e122c1a2888c344d1e5841933cdd22210cdf3
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2022-11-28 23:21:10 +0800

    partial save booklist when import sqlite

--- index.js
+++ index.js
@@ -162,6 +162,7 @@ let loadBookListFromBrFile = async ()=>{
 }
 
 let saveBookListToBrFile = async (data)=>{
+  console.log('Saved BookList')
   let buffer = await promisify(brotliCompress)(JSON.stringify(data))
   return await fs.promises.writeFile(path.join(STORE_PATH, 'bookList.json.br'), buffer)
 }
@@ -684,31 +685,36 @@ ipcMain.handle('import-sqlite', async(event, bookList)=>{
       let bookListLength = bookList.length
       for (let i = 0; i < bookListLength; i++) {
         let book = bookList[i]
-        let metadata = await db.get('SELECT * FROM gallery WHERE thumb LIKE ?', `%${book.coverHash}%`)
-        if (metadata) {
-          metadata.tags = {
-            artist: metadata.artist ? JSON.parse(metadata.artist.replace(re, '\"')) : undefined,
-            group: metadata.group ? JSON.parse(metadata.group.replace(re, '\"')) : undefined,
-            parody: metadata.parody ? JSON.parse(metadata.parody.replace(re, '\"')) : undefined,
-            character: metadata.character ? JSON.parse(metadata.character.replace(re, '\"')) : undefined,
-            female: metadata.female ? JSON.parse(metadata.female.replace(re, '\"')) : undefined,
-            male: metadata.male ? JSON.parse(metadata.male.replace(re, '\"')) : undefined,
-            language: metadata.language ? JSON.parse(metadata.language.replace(re, '\"')) : undefined,
-            mixed: metadata.mixed ? JSON.parse(metadata.mixed.replace(re, '\"')) : undefined,
-            other: metadata.other ? JSON.parse(metadata.other.replace(re, '\"')) : undefined,
-            cosplayer: metadata.cosplayer ? JSON.parse(metadata.cosplayer.replace(re, '\"')) : undefined,
-            rest: metadata.rest ? JSON.parse(metadata.rest.replace(re, '\"')) : undefined,
+        if (book.status !== 'tagged') {
+          let metadata = await db.get('SELECT * FROM gallery WHERE thumb LIKE ?', `%${book.coverHash}%`)
+          if (metadata) {
+            metadata.tags = {
+              artist: metadata.artist ? JSON.parse(metadata.artist.replace(re, '\"')) : undefined,
+              group: metadata.group ? JSON.parse(metadata.group.replace(re, '\"')) : undefined,
+              parody: metadata.parody ? JSON.parse(metadata.parody.replace(re, '\"')) : undefined,
+              character: metadata.character ? JSON.parse(metadata.character.replace(re, '\"')) : undefined,
+              female: metadata.female ? JSON.parse(metadata.female.replace(re, '\"')) : undefined,
+              male: metadata.male ? JSON.parse(metadata.male.replace(re, '\"')) : undefined,
+              language: metadata.language ? JSON.parse(metadata.language.replace(re, '\"')) : undefined,
+              mixed: metadata.mixed ? JSON.parse(metadata.mixed.replace(re, '\"')) : undefined,
+              other: metadata.other ? JSON.parse(metadata.other.replace(re, '\"')) : undefined,
+              cosplayer: metadata.cosplayer ? JSON.parse(metadata.cosplayer.replace(re, '\"')) : undefined,
+              rest: metadata.rest ? JSON.parse(metadata.rest.replace(re, '\"')) : undefined,
+            }
+            metadata.filecount = +metadata.filecount
+            metadata.rating = +metadata.rating
+            metadata.posted = +metadata.posted
+            metadata.filesize = +metadata.filesize
+            metadata.url = `https://exhentai.org/g/${metadata.gid}/${metadata.token}/`
+            _.assign(book, _.pick(metadata, ['tags', 'title', 'title_jpn', 'filecount', 'rating', 'posted', 'filesize', 'category', 'url']), {status: 'tagged'})
+            sendMessageToWebContents(`${i+1} of ${bookListLength}, found metadata for ${book.filepath}`)
+            mainWindow.setProgressBar(i/bookListLength)
+          } else {
+            sendMessageToWebContents(`${i+1} of ${bookListLength}, metadata not found for ${book.filepath}`)
           }
-          metadata.filecount = +metadata.filecount
-          metadata.rating = +metadata.rating
-          metadata.posted = +metadata.posted
-          metadata.filesize = +metadata.filesize
-          metadata.url = `https://exhentai.org/g/${metadata.gid}/${metadata.token}/`
-          _.assign(book, _.pick(metadata, ['tags', 'title', 'title_jpn', 'filecount', 'rating', 'posted', 'filesize', 'category', 'url']), {status: 'tagged'})
-          sendMessageToWebContents(`${i+1} of ${bookListLength}, found metadata for ${book.filepath}`)
-          mainWindow.setProgressBar(i/bookListLength)
-        } else {
-          sendMessageToWebContents(`${i+1} of ${bookListLength}, metadata not found for ${book.filepath}`)
+        }
+        if ((i+1) % 100 == 0) {
+          await saveBookListToBrFile(bookList)
         }
       }
       await db.close()

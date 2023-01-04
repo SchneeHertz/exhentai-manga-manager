78885685afef797ba3c8cf552e8ff1e55a718fad
SchneeHertz <snowhzhang@gmail.com>
2023-01-04 09:24:49 +0800

    remove random recommand

--- src/App.vue
+++ src/App.vue
@@ -1490,14 +1490,14 @@ export default defineComponent({
       })
     },
     geneRecommend (chinese = false, type = 'exhentai') {
-      let tagGroup1 = _.filter(this.displayNodeData, n=>n.size < 200)
+      // let tagGroup1 = _.filter(this.displayNodeData, n=>n.size < 200)
       let tagGroup2 = _.filter(this.displayNodeData, n=>n.size >= 200)
-      let tagGroup3 = []
-      if (tagGroup2.length >= 3) {
-        tagGroup3 = tagGroup2
-      } else {
-        tagGroup3 = [...tagGroup2, ..._.sampleSize(tagGroup1, 3 - tagGroup2.length)]
-      }
+      let tagGroup3 = tagGroup2
+      // if (tagGroup2.length >= 3) {
+      //   tagGroup3 = tagGroup2
+      // } else {
+      //   tagGroup3 = [...tagGroup2, ..._.sampleSize(tagGroup1, 3 - tagGroup2.length)]
+      // }
       if (type === 'exhentai') {
         ipcRenderer['open-url'](`https://exhentai.org/?f_search=${tagGroup3.map(n=>n.name).join(' ')}${chinese?' chinese':''}`)
       } else {

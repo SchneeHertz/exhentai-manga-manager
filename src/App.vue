53bf513fdea66e2edacf7d5d6c1f4f49ce344446
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2024-07-01 20:03:36 +0800

    minor edit

--- src/App.vue
+++ src/App.vue
@@ -199,7 +199,7 @@
       >
         <el-popover
           v-for="book in visibleChunkDisplayBookListForEditTagView" :key="book.id"
-          placement="bottom" :width="400" trigger="hover" :show-after="500" :hide-after="100"
+          placement="left" :width="400" trigger="hover" :show-after="1000" :hide-after="100"
         >
           <template #reference>
             <el-badge

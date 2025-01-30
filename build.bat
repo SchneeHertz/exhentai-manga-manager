f642c8a58b0e8f83b55bf6aa096b0ed75ef7c450
yu45020 <yu45020@gmail.com>
2025-01-30 12:29:06 -0800

    update ignore and build script in windows

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Check if running as administrator
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+:: Move to the directory where your project resides
+cd /d "%~dp0"
+
+:: Run the npm command
+echo Running "npm run dist" with administrator privileges...
+npm run dist
+
+pause
